8621dd7da4346619324e1f599fd5a36bfb98460a
Marco <marcoverbeek99@gmail.com>
2021-04-12 18:47:13 +0200

    Heavily improved analysis performance

--- api/controllers/analysis_controller.js
+++ api/controllers/analysis_controller.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const _ = require('lodash');
 const db = require("../../src/database");
 
-const {limit, calculateGain, getChampionNameById, format} = require("../../utils/analysis_helper")
+const {limit, calculateGain, getChampionNameById, format, performMatchAnalysis} = require("../../utils/analysis_helper")
 const {leagueJs} = require('../../src/leagueJS_setup')
 
 exports.match_analysis = async function (req, res){
@@ -17,145 +17,10 @@ exports.match_analysis = async function (req, res){
         return leagueJs.Match.gettingById(matchesData["matches"][0]["gameId"])
     })
     .then(matchData => {
-        let matchAnalysisResult = []
-
-        // TODO: cleanup. No need to forEach participants, calculate everything once.
-        _.forEach(matchData["participantIdentities"], function (participant) {
-            matchAnalysisResult.push(performAnalysis(matchData, participant["player"]["accountId"]))
-        })
-
-        return res.json(matchAnalysisResult).status(200).end();
+        return res.json(performMatchAnalysis(matchData)).status(200).end();
     })
-    .catch(() => {
+    .catch(err => {
+        console.log(err)
         return res.json({"error": "Could not find summoner by name"}).status(500).end()
     })
-}
-
-/**
- * Analyses a specific match from its data. Requires an accountId in order to calculate LP Gain.
- * <br/> This function has a complex behaviour and an in-depth description is on my TODO list.
- * @param matchData the matchId returned by Riot API
- * @param accountId the player-we-want-to-analyse's accountId
- * @returns {{error: string}|{teamAvgDeaths: number, playerKills: number, playerDeaths: number, teamComparedHealedGain: number, playerDamageTaken: number, teamAvgDamageDone: number, teamComparedDamageGain: number, teamComparedTankedGain: number, summonerName, championPlayed: string, playerAssists: number, teamComparedKA: number, teamAvgKA: number, teamComparedDeaths: number, teamComparedKAGain: number, teamAvgDamageTaken: number, playerKA: number, playerDamageDone: number, teamAvgHealed: number, lpGain: number, teamComparedDeathsGain: number, teamComparedBestDTH: unknown, winConditionGain: number, playerHealed: number}}
- */
-const performAnalysis = (matchData, accountId) => {
-    if (matchData["queueId"] !== 450)
-        return {"error": "queueId was not ARAM"}
-
-    const participantIdentity = _.find(matchData["participantIdentities"], {player: {accountId: accountId}})
-    const participantId = participantIdentity["participantId"]
-    const summonerName = participantIdentity["player"]["summonerName"]
-
-    const participant = _.find(matchData["participants"], {participantId: participantId})
-    const teamId = participant["teamId"]
-    const win = participant["stats"]["win"]
-    const championId = participant["championId"]
-    const championName = getChampionNameById(championId)
-
-    let allyTotalKills = 0, allyTotalDeaths = 0, allyTotalAssists = 0, allyTotalDamage = 0, allyTotalTanked = 0,
-        allyTotalHealed = 0
-    let playerKills = 0, playerDeaths = 0, playerAssists = 0, playerKA = 0, playerDamage = 0, playerTanked = 0,
-        playerHealed = 0
-
-    _.forEach(matchData["participants"], function (value) {
-        if (value["teamId"] === teamId) {
-            allyTotalKills += value["stats"]["kills"]
-            allyTotalDeaths += value["stats"]["deaths"]
-            allyTotalAssists += value["stats"]["assists"]
-
-            allyTotalDamage += value["stats"]["totalDamageDealtToChampions"]
-            allyTotalTanked += value["stats"]["totalDamageTaken"]
-            allyTotalHealed += value["stats"]["totalHeal"]
-
-            if (value["participantId"] === participantId) {
-                playerKills = value["stats"]["kills"]
-                playerDeaths = value["stats"]["deaths"]
-                playerAssists = value["stats"]["assists"]
-
-                playerKA = playerKills + playerAssists
-
-                playerDamage = value["stats"]["totalDamageDealtToChampions"]
-                playerTanked = value["stats"]["totalDamageTaken"]
-                playerHealed = value["stats"]["totalHeal"]
-            }
-
-            //get raram rank, add to allyTotalRank
-        } else {
-            //get raram rank, add to enemyTotalRank
-        }
-    })
-
-    const allyAvgDeaths = allyTotalDeaths / 5
-    const allyAvgKA = (allyTotalKills + allyTotalAssists) / 5
-
-    const allyAvgDamage = allyTotalDamage / 5
-    const allyAvgTanked = allyTotalTanked / 5
-    const allyAvgHealed = allyTotalHealed / 5
-
-    const teamComparedDeaths = format((playerDeaths - allyAvgDeaths) / allyAvgDeaths)
-    const teamComparedKA = format((playerKA - allyAvgKA) / allyAvgKA)
-
-    const teamComparedDamage = (playerDamage - allyAvgDamage) / allyAvgDamage
-    const teamComparedTanked = (playerTanked - allyAvgTanked) / allyAvgTanked
-    const teamComparedHealed = (playerHealed - allyAvgHealed) / allyAvgHealed
-
-    // Win condition
-    const winGain = win ? 10 : -10
-
-    // Kills + Assists compared to team average
-    const teamComparedKAGain = limit(calculateGain(teamComparedKA, 10, 2))
-
-    // Deaths compared to team average
-    const teamComparedDeathsGain = limit(calculateGain(teamComparedDeaths)) * -1
-
-    // Damage/Tanked/Healed compared to team average
-    const teamComparedDamageGain = limit(calculateGain(teamComparedDamage))
-    const teamComparedTankedGain = limit(calculateGain(teamComparedTanked))
-    const teamComparedHealedGain = limit(calculateGain(teamComparedHealed))
-
-    const teamComparedBestOfTDH = _.max([teamComparedDamageGain, teamComparedTankedGain, teamComparedHealedGain])
-
-    let lpGain = winGain + teamComparedKAGain + teamComparedDeathsGain + teamComparedBestOfTDH
-    lpGain = parseFloat(lpGain.toFixed(2))
-
-    // Add LP to player, if he exists in DB.
-    db.query('SELECT account_id FROM raram.users WHERE summoner_name = $1', [summonerName])
-    .then(result => {
-        if(result.rowCount !== 0)
-            db.query('UPDATE raram.users SET lp = lp + $1 WHERE account_id = $2', [lpGain, result.rows[0]["account_id"]])
-    })
-
-    return {
-        "summonerName": summonerName,
-        "championPlayed": championName,
-        "playerKills": playerKills,
-        "playerDeaths": playerDeaths,
-        "playerAssists": playerAssists,
-        "playerKA": playerKA,
-        "teamAvgKA": allyAvgKA,
-        "teamAvgDeaths": allyAvgDeaths,
-        "teamComparedKA": teamComparedKA,
-        "teamComparedDeaths": teamComparedDeaths,
-        "winConditionGain": winGain,
-        "teamComparedDeathsGain": teamComparedDeathsGain,
-        "teamComparedKAGain": teamComparedKAGain,
-        "playerDamageDone": playerDamage,
-        "playerDamageTaken": playerTanked,
-        "playerHealed": playerHealed,
-        "teamAvgDamageDone": allyAvgDamage,
-        "teamAvgDamageTaken": allyAvgTanked,
-        "teamAvgHealed": allyAvgHealed,
-        "teamComparedDamageGain": teamComparedDamageGain,
-        "teamComparedTankedGain": teamComparedTankedGain,
-        "teamComparedHealedGain": teamComparedHealedGain,
-        "teamComparedBestDTH": teamComparedBestOfTDH,
-        "lpGain": lpGain,
-    }
-
-    // determine average of enemy team rank & ally team rank
-    // if win  && avgEnemyRank > avgAllyRank: lp += getAmountOfDivisionsBetween(avgEnemyRank, avgAllyRank)
-    // if lose && avgEnemyRank < avgAllyRank: lp -= getAmountOfDivisionsBetween(avgEnemyRank, avgAllyRank)
-
-    // set lp from db.users to actual + lp-gain
-    // add match.gameId, accountId, lp-gain to db.
 }
--- utils/analysis_helper.js
+++ utils/analysis_helper.js
@@ -25,7 +25,7 @@ const mapChampionNameToId = () => {
  * @param max The highest amount allowed
  * @returns {number} gain if between min and max, min if lower than min, max if higher than max
  */
-exports.limit = (gain = 0, min = -4, max = 4) => {
+const limit = (gain = 0, min = -4, max = 4) => {
     return gain < 0 ? _.max([min, gain]) : _.min([gain, max])
 }
 
@@ -37,8 +37,8 @@ exports.limit = (gain = 0, min = -4, max = 4) => {
  * @param resultMultiplier result of previous operation will be multiplied with resultMultiplier
  * @returns {number} 2-decimal float representing (gain*multiplier) * resultMultiplier
  */
-exports.calculateGain = (gain, multiplier = 10, resultMultiplier = 1) => {
-    return (gain * 10).toFixed(2) * resultMultiplier
+const calculateGain = (gain, multiplier = 10, resultMultiplier = 1) => {
+    return limit((gain * 10).toFixed(2) * resultMultiplier)
 }
 
 /**
@@ -52,7 +52,7 @@ const ChampionList = mapChampionNameToId()
  * @param id champion id
  * @returns {string} the champion's name.
  */
-exports.getChampionNameById = (id) => {
+const getChampionNameById = (id) => {
     return ChampionList.get(parseInt(id)) ?? "Not found"
 }
 
@@ -61,6 +61,119 @@ exports.getChampionNameById = (id) => {
  * @param value the value you wish to format
  * @returns {number} formatted value
  */
-exports.format = (value) => {
+const format = (value) => {
     return parseFloat(value.toFixed(2))
+}
+
+/**
+ * Finds a summoner's Identity from its participantId
+ * @param matchData match data returned by Riot's API
+ * @param participantId function finds identity from this participantId
+ * @returns {JSON} representing a summonerDTO (see Riot's API documentation)
+ */
+function getParticipantIdentity(matchData, participantId) {
+    return _.find(matchData["participantIdentities"], {participantId: participantId})
+}
+
+/**
+ * Analyses a game coming from Riot's API
+ * @param matchData comes from a GET on /lol/match/v4/matches/{matchId}
+ * @returns {{teams: *[], players: *[]}} an array filled with team- and playerdata.
+ */
+exports.performMatchAnalysis = (matchData) => {
+    const players = []
+    const teams = []
+
+    _.forEach(matchData["participants"], function(e){
+        // Participant-related information
+        const participant = {}
+
+        const identity = getParticipantIdentity(matchData, e["participantId"])
+        participant["accountId"] = identity["player"]["accountId"]
+        participant["summonerName"] = identity["player"]["summonerName"]
+
+        participant["champion"] = getChampionNameById(e["championId"])
+        participant["teamId"] = e["teamId"]
+
+        participant["kills"] = e["stats"]["kills"]
+        participant["deaths"] = e["stats"]["deaths"]
+        participant["assists"] = e["stats"]["assists"]
+
+        participant["KA"] = e["stats"]["kills"] + e["stats"]["assists"]
+
+        participant["damageDone"] = e["stats"]["totalDamageDealtToChampions"]
+        participant["damageTaken"] = e["stats"]["totalDamageTaken"]
+        participant["healed"] = e["stats"]["totalHeal"]
+
+        players.push(participant)
+
+        // Team-related information
+        let team = _.find(teams, {teamId: e["teamId"]})
+        if(team === undefined){
+            team = {
+                "teamId": e["teamId"],
+                "win": e["stats"]["win"],
+                "totalKills": e["stats"]["kills"],
+                "totalAssists": e["stats"]["assists"],
+                "totalDeaths": e["stats"]["deaths"],
+                "totalDamageDone": e["stats"]["totalDamageDealtToChampions"],
+                "totalDamageTaken": e["stats"]["totalDamageTaken"],
+                "totalHealed": e["stats"]["totalHeal"],
+            }
+
+            teams.push(team)
+        } else {
+            team["totalKills"] += e["stats"]["kills"]
+            team["totalAssists"] += e["stats"]["assists"]
+            team["totalDeaths"] += e["stats"]["deaths"]
+            team["totalDamageDone"] += e["stats"]["totalDamageDealtToChampions"]
+            team["totalDamageTaken"] += e["stats"]["totalDamageTaken"]
+            team["totalHealed"] += e["stats"]["totalHeal"]
+        }
+    })
+
+    // Calculate team-averages
+    _.forEach(teams, function(team){
+        team["avgKA"] = (team["totalKills"] + team["totalAssists"]) / 5
+        team["avgDeaths"] = team["totalDeaths"] / 5
+
+        team["avgDamageDone"] = team["totalDamageDone"] / 5
+        team["avgDamageTaken"] = team["totalDamageTaken"] / 5
+        team["avgHealed"] = team["totalHealed"] / 5
+    })
+
+    // Calculate team-comparisons and gains and total LP
+    _.forEach(players, function(player){
+        const team = _.find(teams, {teamId: player["teamId"]})
+
+        player["teamComparedKA"] = format((player["KA"] - team["avgKA"]) / team["avgKA"])
+        player["teamComparedDeaths"] = format((player["deaths"] - team["avgDeaths"]) / team["avgDeaths"])
+
+        player["teamComparedDamageDone"] = format((player["damageDone"] - team["avgDamageDone"]) / team["avgDamageDone"])
+        player["teamComparedDamageTaken"] = format((player["damageTaken"] - team["avgDamageTaken"]) / team["avgDamageTaken"])
+        player["teamComparedHealed"] = format((player["healed"] - team["avgHealed"]) / team["avgHealed"])
+
+        player["KAGain"] = calculateGain(player["teamComparedKA"], 10, 2)
+        player["deathsGain"] = calculateGain(player["teamComparedDeaths"], 10, -1)
+
+        player["damageDoneGain"] = calculateGain(player["teamComparedDamageDone"])
+        player["damageTakenGain"] = calculateGain(player["teamComparedDamageTaken"])
+        player["healedGain"] = calculateGain(player["teamComparedHealed"])
+
+        player["lpGain"] = format((team["win"] ? 10 : -10) + player["KAGain"] + player["deathsGain"] + (_.max([player["damageDoneGain"], player["damageTakenGain"], player["healedGain"]])))
+    })
+
+    return {
+        "teams": teams,
+        "players": players
+    }
+
+    /*
+    // Add LP to player, if he exists in DB.
+    db.query('SELECT account_id FROM raram.users WHERE summoner_name = $1', [summonerName])
+    .then(result => {
+        if(result.rowCount !== 0)
+            db.query('UPDATE raram.users SET lp = lp + $1 WHERE account_id = $2', [lpGain, result.rows[0]["account_id"]])
+    })
+     */
 }
