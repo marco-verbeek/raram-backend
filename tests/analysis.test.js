1a796a1816c987e0215ad49468a6fc12796d8925
Marco <marcoverbeek99@gmail.com>
2021-04-20 16:34:25 +0200

    Messing with code reformat

--- tests/analysis.test.js
+++ tests/analysis.test.js
@@ -1,16 +1,14 @@
-const app = require("../src/index");
-const request = require("supertest");
-const {getChampionNameById} = require("../utils/analysis_helper");
+const app = require('../src/index')
+const request = require('supertest')
+const { getChampionNameById } = require('../utils/analysis_helper')
 
 it('finds champions by their id', () => {
-    expect(getChampionNameById(266)).toBe("Aatrox");
-    expect(getChampionNameById(143)).toBe("Zyra");
-});
+  expect(getChampionNameById(266)).toBe('Aatrox')
+  expect(getChampionNameById(143)).toBe('Zyra')
+})
 
 it('returns valid json data from analysed match', () => {
-    return request(app)
-        .get('/api/analysis')
-        .then(response => {
-            expect(response.statusCode).toBe(200);
-        });
+  return request(app).get('/api/analysis').then(response => {
+    expect(response.statusCode).toBe(200)
+  })
 })
